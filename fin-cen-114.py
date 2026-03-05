80cf6857a907834dc63d15a7531d30db553b2f96
Jason Gullifer <jason.gullifer@gmail.com>
2026-03-05 18:20:11 -0500

    - Broke main code out into functions (filter_subaccounts, build_reportable, find_daily_max, get_cli_args) - Added subaccount functionality (filter_subaccounts logic, logic in build_reportable). User can specificy subaccount names at run (--subaccounts flag), and account leafs below that name will be aggregated. Useful if you have "theoretical" sub accounts of a real account. - Added assertion to iter_year to make sure postings are in sorted order by date. - Added formatted printing to CLI output table.

--- fin-cen-114.py
+++ fin-cen-114.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-
+import argparse
 import logging
 import sys
 import functools
 import math
 import collections
 import datetime
+import heapq
 import beancount.loader
 import beancount.utils
 import beancount.core
@@ -13,6 +14,8 @@ import beancount.core.realization
 import beancount.core.data
 import beancount.parser
 from beancount.utils.date_utils import iter_dates
+from beancount.core.number import D
+from beancount.core.amount import Amount, add
 import pandas as pd
 
 def get_date(p):
@@ -30,7 +33,7 @@ def only_postings(p):
         return True
     else:
         return False
-
+        
 def this_year(year, p):
     return get_date(p).year == year
 
@@ -50,13 +53,18 @@ def start_of_year_inventory(year, postings):
     return balance
 
 def iter_year(year, account_postings, inventory, price_map):
+    assert all(
+        get_date(a) <= get_date(b)
+        for a, b in zip(account_postings, account_postings[1:])
+    ), "account_postings must be sorted by date"
     start_of_year = datetime.date(year, 1, 1)
     end_of_year = datetime.date(year+1, 1, 1)
 
     filter_year = functools.partial(this_year, year)
 
-    postings = filter(only_postings, account_postings)
-    postings = filter(filter_year, postings)
+    postings = account_postings
+    # postings = sorted(filter(only_postings, account_postings), key=get_date)
+    postings = filter(filter_year, iter(postings))
     txn = next(postings, None)
     for date in iter_dates(start_of_year, end_of_year):
         while txn and get_date(txn) <= date:
@@ -73,69 +81,116 @@ def get_account_number(account, keys):
 def fmt_d(n):
     return '${:,.0f}'.format(n)
 
-if __name__ == '__main__':
-    import argparse
+def filter_subaccounts(subaccts, accounts_sorted):
+    subaccounts_sorted = {}    
+    accounts_filtered = []
+
+    for major_account_name in subaccts:
+        for ix, a in enumerate(accounts_sorted):
+            minor_account_name = a[0]
+            if major_account_name in minor_account_name:
+                if major_account_name not in subaccounts_sorted:
+                    subaccounts_sorted[major_account_name] = [a]                    
+                else:
+                    subaccounts_sorted[major_account_name].append(a)
+                print(f"Found subaccount: {minor_account_name:<50s}")
+            else:
+                accounts_filtered.append(a)
+    print()
+    return subaccounts_sorted, accounts_filtered
+
+def build_reportable(accounts_sorted, subaccounts, realized_accounts, year, only_account=None):
+    if subaccounts:
+        subaccounts_sorted, accounts_sorted = filter_subaccounts(subaccounts, accounts_sorted)
+
+    reportable = []
+    for account, (open, close) in accounts_sorted:
+        if only_account and account not in only_account: 
+            continue
+        open_year = open.date.year if open else -math.inf
+        close_year = close.date.year if close else math.inf
+        if open_year <= year <= close_year:
+            # reportable.append((account, open, list(realized_accounts[account])))
+            reportable.append((account, open, [p for p in realized_accounts[account] if only_postings(p)]))
+
+    if subaccounts:
+        streams=[]
+        for major_account, minor_accounts in subaccounts_sorted.items():
+            postings = []
+            last_open = None
+            for account, (open, close) in minor_accounts:
+                if only_account and account not in only_account: 
+                    continue
+                open_year = open.date.year if open else -math.inf
+                close_year = close.date.year if close else math.inf
+                if open_year <= year <= close_year:
+                    streams.append([p for p in realized_accounts[account] if only_postings(p)])
+                    last_open = open
+            postings = list(heapq.merge(*streams, key=get_date))
+            if postings:
+                reportable.append((major_account, last_open, postings))
+
+    return reportable
+
+def find_daily_max(year, postings, price_map):
+    inventory = start_of_year_inventory(year, postings)
+    max_value_date = None
+    max_value = 0
+    max_value_cad = 0
+    for date, balance_usd, balance_cad in iter_year(year, postings, inventory, price_map):
+        usd_value = balance_usd.get_currency_units('USD')
+        cad_value = balance_cad.get_currency_units('CAD')
+        if int(usd_value.number) > max_value:
+            max_value = int(usd_value.number)
+            max_value_cad = int(cad_value.number)
+            max_value_date = date
+    return max_value, max_value_cad, max_value_date
+
+def get_cli_args():
     parser = argparse.ArgumentParser(
         description="Summarise account information for FinCEF 114 filing."
     )
     parser.add_argument('bean', help='Path to the beancount file.')
     parser.add_argument('--year', type=int, help='Which year to summarise.', required=True)
+    parser.add_argument('--subaccounts', action='append', help='Group sub-accounts under this parent account (repeatable).')
     parser.add_argument('--only-account', action='append', help='Only calculate for specified account(s).')
     parser.add_argument('--meta-account-number', default=['account-number'], action='append', help='Metadata key(s) containing account numbers.')
     args = parser.parse_args()
+    return args
 
+if __name__ == '__main__':
+    args = get_cli_args()
     entries, errors, options = beancount.loader.load_file(args.bean, logging.info, log_errors=sys.stderr)
 
     account_types = beancount.parser.options.get_account_types(options)
     open_close = beancount.core.getters.get_account_open_close(entries)
 
-    filter_fun = functools.partial(beancount.core.account_types.is_account_type, account_types.assets)
-    sortkey_fun = functools.partial(beancount.core.account_types.get_account_sort_key, account_types)
+    # We only want Asset accounts and we want them sorted by account name
+    filterby_assets = functools.partial(beancount.core.account_types.is_account_type, account_types.assets)
+    sortby_name = functools.partial(beancount.core.account_types.get_account_sort_key, account_types)
 
-    # We only want Asset accounts and we want them sorted for us
     items = open_close.items()
-    accounts_filtered = filter(lambda entry: filter_fun(entry[0]), items)
-    accounts_sorted = sorted(accounts_filtered, key=lambda entry: sortkey_fun(entry[0]))
+    accounts_filtered = filter(lambda entry: filterby_assets(entry[0]), items)
+    accounts_sorted = sorted(accounts_filtered, key=lambda entry: sortby_name(entry[0]))
 
     realized_accounts = beancount.core.realization.postings_by_account(entries)
     price_map = beancount.core.prices.build_price_map(entries)
 
+    reportable = build_reportable(accounts_sorted, args.subaccounts, realized_accounts, args.year, args.only_account)
+
     rows = []
+    print(f"{'Account name':50s}\t{'CAD':>12s}\t{'USD':>12s}\t{'Acct. number':>12s}")
+    for display_name, open_directive, postings in reportable:
+        max_value, max_value_cad, max_value_date = find_daily_max(args.year, postings, price_map)
+        account_number = get_account_number(open_directive, args.meta_account_number) if open_directive else ''
+        rows.append({
+            "account": display_name,
+            "account_number": account_number,
+            "date": max_value_date,
+            "cad": max_value_cad,
+            "usd": max_value
+        })
+        print(f"{display_name:50s}\t${(max_value_cad):11,.0f}\t${(max_value):11,.0f}\t{account_number:>12}")
 
-    for account, (open, close) in accounts_sorted:
-        if args.only_account and account not in args.only_account: continue
-
-        open_year = open.date.year if open else -math.inf
-        close_year = close.date.year if close else math.inf
-
-        # Only do this for accounts that were open during the year in question
-        if open_year <= args.year <= close_year:
-            account_postings = realized_accounts[account]
-            inventory = start_of_year_inventory(args.year, account_postings)
-            
-            # now we need to iterate day-by-day through the year. We add any new
-            # postings and then reduce to a price. Then see if that is the highest
-            # price seen so far....
-            max_value_date = None
-            max_value = 0
-            max_value_cad = 0
-            for (date, balance_usd, balance_cad) in iter_year(args.year, account_postings, inventory, price_map):
-                # breakpoint()
-                usd_value = balance_usd.get_currency_units('USD')
-                cad_value = balance_cad.get_currency_units('CAD')
-                if int(usd_value.number)>max_value:
-                    max_value = max(max_value, int(usd_value.number))
-                    max_value_cad = max(max_value_cad, int(cad_value.number))
-                    max_value_date = date
-
-            account_number = get_account_number(open, args.meta_account_number)
-            rows.append({
-                "account": account,
-                "account_number": account_number,
-                "date": max_value_date,
-                "cad": max_value_cad,
-                "usd": max_value
-            })
-            df = pd.DataFrame(rows)
-            df.to_csv("summary.csv", index=False)
-            print(account, 'CAD: ', fmt_d(max_value_cad), 'USD: ', fmt_d(max_value), account_number)
+    df = pd.DataFrame(rows)
+    df.to_csv("summary.csv", index=False)#!/usr/bin/env python
